c827ada1d9cf91634ffefef1556c6fd970ea029d
pdwood <woodp@rpi.edu>
2017-07-24 21:08:20 -0400

    Fixed overridden properties issue

--- backend/ontosend.py
+++ backend/ontosend.py
@@ -33,7 +33,10 @@ def add_concepts_from_tree(node):
       "inheritedProperties" : [ ]
     }
     props = entry["localProperties"]
+    overr = entry["overriddenFillers"]
 
+    parent = onto.find_one({"name":event["concept"].lower()})
+    
     for child in node.children:
       if len(child.children) > 0:
         add_concepts_from_tree(child)
@@ -42,11 +45,31 @@ def add_concepts_from_tree(node):
     if "THEME" in event:
       props.append( {"filler" : node.tmr[event["THEME"]]["concept"].lower(), "slot" : "theme", "facet" : "sem"} )
       if "THEME-1" in event:
-            props.append( {"filler" : node.tmr[event["THEME-1"]]["concept"].lower(), "slot" : "theme", "facet" : "sem"} )
+        props.append( {"filler" : node.tmr[event["THEME-1"]]["concept"].lower(), "slot" : "theme", "facet" : "sem"} )
+        
+      found_ancestor = False
+      ancestor = parent
+      while not found_ancestor:
+        for property in ancestor["localProperties"]:
+          if property["slot"] == "theme" and property["facet"] == "sem":
+            overr.append(property)
+            found_ancestor = True
+        ancestor = onto.find_one({"name":ancestor["parents"][0]}) # TODO support multiple inheritance
+          
     if "INSTRUMENT" in event:
       props.append( {"filler" : node.tmr[event["INSTRUMENT"]]["concept"].lower(), "slot" : "instrument", "facet" : "sem"} )
+      
+      found_ancestor = False
+      ancestor = parent
+      while not found_ancestor:
+        for property in ancestor["localProperties"]:
+          if property["slot"] == "instrument" and property["facet"] == "sem":
+            overr.append(property)
+            found_ancestor = True
+        ancestor = onto.find_one({"name":ancestor["parents"][0]}) # TODO support multiple inheritance
     
-    #Apparently I need to have overridden fillers? idk
+    #TODO add the overridden fillers
+    #These should be case roles that are ancestors of this thing's case role (e.g. ARTIFACT for CHAIR)
 
     onto.insert_one(entry)
     assert(onto.find_one({"name":name}))
@@ -56,3 +79,6 @@ def add_concepts_from_tree(node):
       if len(child.children) > 0:
         add_concepts_from_tree(child)
         onto.update_one({"name":name},{"$push":{"localProperties":{"facet":"sem","slot":"has-event-as-part","filler":event_name_to_concept_name(child.name)}}})
+        
+        
+# TODO remove duplicate event-as-parts
