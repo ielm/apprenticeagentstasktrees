305dfb4df3d82ec4f2b69c39c19fa44efd316f2a
Jesse <jesse.english@southerncrossengineering.com>
2018-06-12 14:30:37 -0400

    (WIP) more tmr work; many things marked as removable for current implementation

--- backend/models/tmr.py
+++ backend/models/tmr.py
@@ -4,6 +4,7 @@ from backend.models.syntax import Syntax
 # from backend.models.tmrinstance import TMRInstance
 from backend.utils.YaleUtils import tmr_action_name
 
+from functools import reduce
 from uuid import uuid4
 
 
@@ -70,76 +71,84 @@ class TMR(Graph):
     def _frame_type(self):
         return TMRInstance
 
-    # TODO: TEST
+    @DeprecationWarning
     def is_action(self):
         event = self.find_main_event()
         if event is None:
             return False
 
         # 1) Is the event a REQUEST-ACTION, where the AGENT = HUMAN, and the BENEFICIARY = ROBOT
-        if event.concept == "REQUEST-ACTION":
-            if "AGENT" in event \
-                    and "BENEFICIARY" in event \
-                    and "HUMAN" in event["AGENT"] \
-                    and "ROBOT" in event["BENEFICIARY"]:
-                return True
+        if event ^ self.ontology + ".REQUEST-ACTION" \
+                and event["AGENT"] ^ self.ontology + ".HUMAN" \
+                and event["BENEFICIARY"] ^ self.ontology + ".ROBOT":
+            return True
+
+        # if event.concept == "REQUEST-ACTION":
+        #     if "AGENT" in event \
+        #             and "BENEFICIARY" in event \
+        #             and "HUMAN" in event["AGENT"] \
+        #             and "ROBOT" in event["BENEFICIARY"]:
+        #         return True
 
         # 2) Is the event a PHYSICAL-EVENT, where the AGENT = HUMAN, and the TIME is present-tense
-        if event.concept == "PHYSICAL-EVENT" or "PHYSICAL-EVENT" in Ontology.ancestors(event.concept):
-            if "HUMAN" in list(map(lambda agent: agent if type(self[agent]) == str else self[agent].concept, event["AGENT"])):
-                if event["TIME"] == ["FIND-ANCHOR-TIME"]:
-                    return True
+        if event ^ self.ontology + ".PHYSICAL-EVENT" \
+                and event["AGENT"] ^ self.ontology + ".HUMAN" \
+                and event["TIME"] == ["FIND-ANCHOR-TIME"]:
+            return True
+
+        # if event.concept == "PHYSICAL-EVENT" or "PHYSICAL-EVENT" in Ontology.ancestors(event.concept):
+        #     if "HUMAN" in list(map(lambda agent: agent if type(self[agent]) == str else self[agent].concept, event["AGENT"])):
+        #         if event["TIME"] == ["FIND-ANCHOR-TIME"]:
+        #             return True
 
         return False
 
-    # TODO: TEST
-    # TODO: rename - we are really testing if this is an HTN composition
+    @DeprecationWarning
     def is_utterance(self):
         return not self.is_action()
 
-    # TODO: TEST
-    def get_name(self):
-        event = self.find_main_event()
-        if event is None:
-            return ""
-
-        if self.is_action():
-            return tmr_action_name(self)
-
-        output = event.concept
-
-        def _name_theme(instance):
-            name = instance.concept
-            if ">" in instance["CARDINALITY"]:
-                name += "s"
-            if "SPATIAL-ORIENTATION" in instance:
-                name = " ".join(instance["SPATIAL-ORIENTATION"]) + " " + name
-            if "SIDE-TB" in instance:
-                name = " ".join(instance["SIDE-TB"]) + " " + name
-            return name
-
-        node = event
-        while "THEME" in node:
-
-            # output += " " + " AND ".join(list(map(lambda theme: self[theme].concept, node["THEME"])))
-            output += " " + " AND ".join(list(map(lambda theme: _name_theme(self[theme]), node["THEME"])))
-            output = output.strip()
-
-            # output += " " + self[node["THEME"]]["concept"]
-            # if "CARDINALITY" in self[node["THEME"]] and self[node["THEME"]]["CARDINALITY"][0] == ">":
-            #     output += "S"
-            # if "THEME-1" in node:
-            #     output += " AND " + self[node["THEME-1"]]["concept"]
-
-            node = self[node["THEME"][0]]
-
-        if "INSTRUMENT" in event:
-            output += " WITH " + " AND ".join(list(map(lambda instrument: self[instrument].concept, event["INSTRUMENT"])))
-
-        if "DESTINATION" in event:
-            output += " TO " + " AND ".join(list(map(lambda destination: self[destination].concept, event["DESTINATION"])))
-
-        return output
+    # def get_name(self):
+    #     event = self.find_main_event()
+    #     if event is None:
+    #         return ""
+    #
+    #     if self.is_action():
+    #         return tmr_action_name(self)
+    #
+    #     output = event.concept
+    #
+    #     def _name_theme(instance):
+    #         name = instance.concept
+    #         if ">" in instance["CARDINALITY"]:
+    #             name += "s"
+    #         if "SPATIAL-ORIENTATION" in instance:
+    #             name = " ".join(instance["SPATIAL-ORIENTATION"]) + " " + name
+    #         if "SIDE-TB" in instance:
+    #             name = " ".join(instance["SIDE-TB"]) + " " + name
+    #         return name
+    #
+    #     node = event
+    #     while "THEME" in node:
+    #
+    #         # output += " " + " AND ".join(list(map(lambda theme: self[theme].concept, node["THEME"])))
+    #         output += " " + " AND ".join(list(map(lambda theme: _name_theme(self[theme]), node["THEME"])))
+    #         output = output.strip()
+    #
+    #         # output += " " + self[node["THEME"]]["concept"]
+    #         # if "CARDINALITY" in self[node["THEME"]] and self[node["THEME"]]["CARDINALITY"][0] == ">":
+    #         #     output += "S"
+    #         # if "THEME-1" in node:
+    #         #     output += " AND " + self[node["THEME-1"]]["concept"]
+    #
+    #         node = self[node["THEME"][0]]
+    #
+    #     if "INSTRUMENT" in event:
+    #         output += " WITH " + " AND ".join(list(map(lambda instrument: self[instrument].concept, event["INSTRUMENT"])))
+    #
+    #     if "DESTINATION" in event:
+    #         output += " TO " + " AND ".join(list(map(lambda destination: self[destination].concept, event["DESTINATION"])))
+    #
+    #     return output
 
     def find_main_event(self):
         event = None
@@ -187,7 +196,7 @@ class TMR(Graph):
 
         return False
 
-    # TODO: TEST
+    @DeprecationWarning
     def has_same_main_event(self, tmr):
         if tmr is None:
             return False
@@ -195,32 +204,67 @@ class TMR(Graph):
         event1 = self.find_main_event()
         event2 = tmr.find_main_event()
 
-        if event1.concept != event2.concept:
+        if event1 is None or event2 is None:
             return False
 
-        if "AGENT" in event1 \
-                and "AGENT" in event2 \
-                and len(set(event1["AGENT"]) - self.keys()) > 0 \
-                and len(set(event2["AGENT"]) - tmr.keys()) > 0 \
-                and len(self.get_concepts(event1["AGENT"]) & tmr.get_concepts(event2["AGENT"])) == 0:
+        if event1["IS-A"] != event2["IS-A"]:
             return False
 
-        if ("THEME" in event1) != ("THEME" in event2):
-            return False
+        caseroles = ["AGENT", "THEME", "INSTRUMENT"]
+        for caserole in caseroles:
+            if caserole in event1 and caserole in event2:
+                # This tests to see if the present frames in the case role of each of the input EVENTs share an
+                # exact same immediate ancestor (not full hierarchy search).
+                # Ex: [HUMAN/EVENT]-1 == HUMAN-2; HUMAN-1 != ANIMAL-1
 
-        if "THEME" in event1 \
-                and "THEME" in event2 \
-                and len(self.get_concepts(event1["THEME"]) & tmr.get_concepts(event2["THEME"])) == 0:
-            return False
+                fillers1 = list(map(lambda filler: filler.resolve(), event1[caserole]))
+                fillers2 = list(map(lambda filler: filler.resolve(), event2[caserole]))
 
-        if "INSTRUMENT" in event1 \
-                and "INSTRUMENT" in event2 \
-                and len(self.get_concepts(event1["INSTRUMENT"]) & tmr.get_concepts(event2["INSTRUMENT"])) == 0:
-            return False
+                concepts1 = list(map(lambda filler: filler["IS-A"], fillers1))
+                concepts2 = list(map(lambda filler: filler["IS-A"], fillers2))
+
+                concepts1 = list(reduce(lambda x, y: x + y, concepts1))
+                concepts2 = list(reduce(lambda x, y: x + y, concepts2))
+
+                concepts1 = set(map(lambda concept: concept._value, concepts1))
+                concepts2 = set(map(lambda concept: concept._value, concepts2))
+
+                if len(concepts1 & concepts2) == 0:
+                    return False
+
+        # if "AGENT" in event1 and "AGENT" in event2:
+        #     if event1["AGENT"] ^ event2["AGENT"]:
+        #         return True
 
         return True
 
-    # TODO: TEST
+        # if event1.concept != event2.concept:
+        #     return False
+        #
+        # if "AGENT" in event1 \
+        #         and "AGENT" in event2 \
+        #         and len(set(event1["AGENT"]) - self.keys()) > 0 \
+        #         and len(set(event2["AGENT"]) - tmr.keys()) > 0 \
+        #         and len(self.get_concepts(event1["AGENT"]) & tmr.get_concepts(event2["AGENT"])) == 0:
+        #     return False
+        #
+        # if ("THEME" in event1) != ("THEME" in event2):
+        #     return False
+        #
+        # if "THEME" in event1 \
+        #         and "THEME" in event2 \
+        #         and len(self.get_concepts(event1["THEME"]) & tmr.get_concepts(event2["THEME"])) == 0:
+        #     return False
+        #
+        # if "INSTRUMENT" in event1 \
+        #         and "INSTRUMENT" in event2 \
+        #         and len(self.get_concepts(event1["INSTRUMENT"]) & tmr.get_concepts(event2["INSTRUMENT"])) == 0:
+        #     return False
+        #
+        # return True
+
+    @DeprecationWarning
+    # Ultimately, this could be replaced by a generic recursive querying capability
     def find_themes(self):
         event = self.find_main_event()
         if event is None:
@@ -229,101 +273,111 @@ class TMR(Graph):
         def __find_themes(instance):
             themes = []
 
-            if "THEME" in instance:
-                for theme in instance["THEME"]:
-                    theme_instance = self[theme]
-                    themes.append(theme_instance)
-                    themes.extend(__find_themes(theme_instance))
+            for theme in instance["THEME"]:
+                theme = theme.resolve()
+                themes.append(theme)
+                themes.extend(__find_themes(theme))
+
+            # if "THEME" in instance:
+            #     for theme in instance["THEME"]:
+            #         theme_instance = self[theme]
+            #         themes.append(theme_instance)
+            #         themes.extend(__find_themes(theme_instance))
 
             return themes
 
-        return list(map(lambda theme: theme.concept, __find_themes(event)))
+        themes = list(map(lambda theme: list(theme["IS-A"]._storage), __find_themes(event)))
+        if len(themes) == 0:
+            return themes
 
-    # TODO: TEST
-    def about_part_of(self, tmr):
-        if self is None or tmr is None:
-            return False
+        themes = list(reduce(lambda x, y: x + y, themes))
+        themes = set(map(lambda filler: filler._value, themes))
 
-        event1 = self.find_main_event()
-        event2 = tmr.find_main_event()
+        return themes
+        # return list(map(lambda theme: theme.concept, __find_themes(event)))
 
-        if event1 is None or event2 is None:
-            return False
-
-        if not ("THEME" in event1 and "THEME" in event2):
-            return False
-
-        themes1 = list(map(lambda theme: self[theme], event1["THEME"]))
-        themes2 = list(map(lambda theme: tmr[theme], event2["THEME"]))
-
-        for theme1 in themes1:
-            for theme2 in themes2:
-                if Ontology.contains(theme2.concept, "HAS-OBJECT-AS-PART", "SEM", theme1.concept):
-                    return True
-                if "CARDINALITY" in theme2 and ">" in theme2["CARDINALITY"] and theme1.concept == theme2.concept:
-                    return True # one is the plural of the other
-
-        return False
+    # def about_part_of(self, tmr):
+    #     if self is None or tmr is None:
+    #         return False
+    #
+    #     event1 = self.find_main_event()
+    #     event2 = tmr.find_main_event()
+    #
+    #     if event1 is None or event2 is None:
+    #         return False
+    #
+    #     if not ("THEME" in event1 and "THEME" in event2):
+    #         return False
+    #
+    #     themes1 = list(map(lambda theme: self[theme], event1["THEME"]))
+    #     themes2 = list(map(lambda theme: tmr[theme], event2["THEME"]))
+    #
+    #     for theme1 in themes1:
+    #         for theme2 in themes2:
+    #             if Ontology.contains(theme2.concept, "HAS-OBJECT-AS-PART", "SEM", theme1.concept):
+    #                 return True
+    #             if "CARDINALITY" in theme2 and ">" in theme2["CARDINALITY"] and theme1.concept == theme2.concept:
+    #                 return True # one is the plural of the other
+    #
+    #     return False
 
     # Here we verify that the event subtypes are broadly the same; concerning only with the immediate children
     # of EVENT in the ontology.
     # Every EVENT subtype in this TMR must also be present in the input TMR = if they are not the same (even a
     # subset), then we must consider these two events distinctly different enough that even a correlation in
     # HAS-OBJECT-AS-PART is not sufficient.
-    # TODO: TEST
-    def about_same_events(self, tmr):
-        if self is None or tmr is None:
-            return False
+    # def about_same_events(self, tmr):
+    #     if self is None or tmr is None:
+    #         return False
+    #
+    #     event1 = self.find_main_event()
+    #     event2 = tmr.find_main_event()
+    #
+    #     if event1 is None or event2 is None:
+    #         return False
+    #
+    #     event_types = Ontology.ontology["EVENT"]["SUBCLASSES"]["VALUE"]
+    #     event1_types = set(Ontology.ancestors(event1.concept, include_self=True)).intersection(set(event_types))
+    #     event2_types = set(Ontology.ancestors(event2.concept, include_self=True)).intersection(set(event_types))
+    #
+    #     return len(event1_types.intersection(event2_types)) == len(event1_types)
 
-        event1 = self.find_main_event()
-        event2 = tmr.find_main_event()
-
-        if event1 is None or event2 is None:
-            return False
-
-        event_types = Ontology.ontology["EVENT"]["SUBCLASSES"]["VALUE"]
-        event1_types = set(Ontology.ancestors(event1.concept, include_self=True)).intersection(set(event_types))
-        event2_types = set(Ontology.ancestors(event2.concept, include_self=True)).intersection(set(event_types))
-
-        return len(event1_types.intersection(event2_types)) == len(event1_types)
-
-    # TODO: TEST
+    # NOTE: Ultimately, replace with a generic querying capability
     def find_by_concept(self, concept):
-        return list(filter(lambda instance: instance.concept == concept, self.values()))
+        instances = list(filter(lambda instance: instance.isa(concept), self.values()))
+        return instances
+        # return list(filter(lambda instance: instance.concept == concept, self.values()))
 
-    # TODO: TEST
-    def find_objects(self):
-        objects = []
-
-        for instance in self.values():
-            if instance.is_object():
-                objects.append(instance.concept)
-
-        return objects
+    # def find_objects(self):
+    #     objects = []
+    #
+    #     for instance in self.values():
+    #         if instance.is_object():
+    #             objects.append(instance.concept)
+    #
+    #     return objects
 
     # Is TMR(1) about any of the THEMEs of any of the Actions(2).
     # "About" in this case will check THEME, INSTRUMENT and DESTINATION.
-    # TODO: TEST
-    def is_about(self, actions):
-        about = []
-        event = self.find_main_event()
+    # def is_about(self, actions):
+    #     about = []
+    #     event = self.find_main_event()
+    #
+    #     if "THEME" in event:
+    #         about.extend(self.get_concepts(event["THEME"]))
+    #     if "INSTRUMENT" in event:
+    #         about.extend(self.get_concepts(event["INSTRUMENT"]))
+    #     if "DESTINATION" in event:
+    #         about.extend(self.get_concepts(event["DESTINATION"]))
+    #
+    #     themes = []
+    #     for action in actions:
+    #         themes.extend(action.find_themes())
+    #
+    #     return len(set(about).intersection(set(themes))) > 0
 
-        if "THEME" in event:
-            about.extend(self.get_concepts(event["THEME"]))
-        if "INSTRUMENT" in event:
-            about.extend(self.get_concepts(event["INSTRUMENT"]))
-        if "DESTINATION" in event:
-            about.extend(self.get_concepts(event["DESTINATION"]))
-
-        themes = []
-        for action in actions:
-            themes.extend(action.find_themes())
-
-        return len(set(about).intersection(set(themes))) > 0
-
-    # TODO: TEST
-    def get_concepts(self, instance_keys):
-        return set(map(lambda instance: self[instance].concept, instance_keys))
+    # def get_concepts(self, instance_keys):
+    #     return set(map(lambda instance: self[instance].concept, instance_keys))
 
 
 class TMRInstance(Frame):
