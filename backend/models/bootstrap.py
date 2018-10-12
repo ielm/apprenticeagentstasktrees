72d8e3448b0389af98f616ec73fc02887aeb3173
Jesse <jesse.english@southerncrossengineering.com>
2018-10-12 13:17:16 -0400

    bootstrap declare knowledge moved various knowledge elements to the bootstrap format

--- backend/models/bootstrap.py
+++ backend/models/bootstrap.py
@@ -1,9 +1,10 @@
 from backend.agent import Agent
 from backend.models.agenda import Goal
 from backend.models.grammar import Grammar
-from backend.models.graph import Filler, Frame, Identifier, Literal, Network
+from backend.models.graph import Filler, Frame, Graph, Identifier, Literal, Network
+from backend.models.ontology import Ontology, OntologyFiller
 from pkgutil import get_data
-from typing import Union
+from typing import List, Union
 
 
 class Bootstrap(object):
@@ -16,7 +17,68 @@ class Bootstrap(object):
             bootstrap()
 
 
-class BootstrapKnowledge(Bootstrap):
+class BootstrapTriple(object):
+
+    def __init__(self, slot: str, filler: Union[Identifier, Literal], facet: str=None):
+        self.slot = slot
+        self.facet = facet
+        self.filler = filler
+
+    def __eq__(self, other):
+        if isinstance(other, BootstrapTriple):
+            return self.slot == other.slot and self.facet == other.facet and self.filler == other.filler
+        return super().__eq__(other)
+
+
+class BootstrapDeclareKnowledge(Bootstrap):
+
+    def __init__(self, network: Network, graph: Union[str, Graph], name: str, index: Union[int, bool]=False, isa: Union[str, Identifier, List[Union[str, Identifier]]]=None, properties: Union[BootstrapTriple, List[BootstrapTriple]]=None):
+        self.network = network
+        self.graph = graph
+        self.name = name
+        self.index = index
+        self.isa = isa
+        self.properties = properties
+
+        if self.isa is None:
+            self.isa = []
+        if not isinstance(self.isa, list):
+            self.isa = [self.isa]
+
+        if self.properties is None:
+            self.properties = []
+        if not isinstance(self.properties, list):
+            self.properties = [self.properties]
+
+    def __call__(self, *args, **kwargs):
+        if isinstance(self.graph, str):
+            self.graph = self.network[self.graph]
+
+        id = self.name
+        if not isinstance(self.index, bool):
+            id = id + "." + str(self.index)
+
+        generate_index = self.index if isinstance(self.index, bool) else False
+
+        frame = self.graph.register(id, isa=self.isa, generate_index=generate_index)
+        for property in self.properties:
+            if isinstance(self.graph, Ontology):
+                frame[property.slot] += OntologyFiller(property.filler, property.facet)
+            else:
+                frame[property.slot] += property.filler
+
+    def __eq__(self, other):
+        if isinstance(other, BootstrapDeclareKnowledge):
+            return self.network == other.network and \
+                    self.graph == other.graph and \
+                    self.name == other.name and \
+                    self.index == other.index and \
+                    self.isa == other.isa and \
+                    self.properties == other.properties
+        return super().__eq__(other)
+
+
+class BootstrapAppendKnowledge(Bootstrap):
 
     def __init__(self, network: Network, frame: Union[Frame, Identifier, str], slot: str, filler: Union[Filler, Identifier, Literal]):
         self.network = network
@@ -38,7 +100,7 @@ class BootstrapKnowledge(Bootstrap):
         self.frame[self.slot] += self.filler
 
     def __eq__(self, other):
-        if isinstance(other, BootstrapKnowledge):
+        if isinstance(other, BootstrapAppendKnowledge):
             return \
                 self.network == other.network and \
                 self.frame == other.frame and \
--- tests/models/BootstrapTestCase.py
+++ tests/models/BootstrapTestCase.py
@@ -1,17 +1,104 @@
-from backend.models.bootstrap import BootstrapKnowledge
-from backend.models.graph import Literal, Network
+from backend.models.bootstrap import BootstrapAppendKnowledge, BootstrapDeclareKnowledge, BootstrapTriple
+from backend.models.graph import Identifier, Literal, Network
 
 import unittest
 
 
-class BootstrapKnowledgeTestCase(unittest.TestCase):
+class BootstrapDeclareKnowledgeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.n = Network()
+        self.g = self.n.register("TEST")
+
+    def test_call_makes_instance(self):
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME")
+        boot()
+
+        self.assertIn("TEST.MYFRAME", self.g)
+
+    def test_call_makes_instance_with_specified_index(self):
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME", index=1)
+        boot()
+
+        self.assertIn("TEST.MYFRAME.1", self.g)
+
+    def test_call_makes_instance_with_generated_index(self):
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME", index=True)
+        boot()
+
+        self.assertIn("TEST.MYFRAME.1", self.g)
+
+    def test_call_makes_instances_with_parent(self):
+        self.g.register("PARENT")
+
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME", isa="TEST.PARENT")
+        boot()
+
+        frame = self.g["MYFRAME"]
+        self.assertTrue(frame ^ "TEST.PARENT")
+
+    def test_call_makes_instances_with_parents(self):
+        self.g.register("PARENT1")
+        self.g.register("PARENT2")
+
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME", isa=["TEST.PARENT1", "TEST.PARENT2"])
+        boot()
+
+        frame = self.g["MYFRAME"]
+        self.assertTrue(frame ^ "TEST.PARENT1")
+        self.assertTrue(frame ^ "TEST.PARENT2")
+
+    def test_call_adds_property(self):
+        prop = BootstrapTriple("MYSLOT", Identifier.parse("ONT.ALL"))
+
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME", properties=[prop])
+        boot()
+
+        frame = self.g["MYFRAME"]
+        self.assertTrue(frame["MYSLOT"] == Identifier.parse("ONT.ALL"))
+
+    def test_call_adds_multiple_properties(self):
+        prop1 = BootstrapTriple("MYSLOT", Literal("ONT.ALL"))
+        prop2 = BootstrapTriple("MYSLOT", Literal(123))
+
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME", properties=[prop1, prop2])
+        boot()
+
+        frame = self.g["MYFRAME"]
+        self.assertTrue(frame["MYSLOT"] == "ONT.ALL")
+        self.assertTrue(frame["MYSLOT"] == 123)
+
+    def test_call_respects_facets_in_ontology(self):
+        from backend.models.ontology import Ontology
+        ontology = self.n.register(Ontology("ONT"))
+
+        prop = BootstrapTriple("MYSLOT", Identifier.parse("ONT.ALL"), facet="SEM")
+
+        boot = BootstrapDeclareKnowledge(self.n, "ONT", "MYFRAME", properties=[prop])
+        boot()
+
+        frame = ontology["MYFRAME"]
+        self.assertTrue(frame["MYSLOT"] == Identifier.parse("ONT.ALL"))
+        self.assertEqual("SEM", frame["MYSLOT"][0]._facet)
+
+    def test_call_ignores_facets_in_non_ontology_graphs(self):
+        prop = BootstrapTriple("MYSLOT", Identifier.parse("ONT.ALL"), facet="does-not-matter")
+
+        boot = BootstrapDeclareKnowledge(self.n, "TEST", "MYFRAME", properties=[prop])
+        boot()
+
+        frame = self.g["MYFRAME"]
+        self.assertTrue(frame["MYSLOT"] == Identifier.parse("ONT.ALL"))
+
+
+class BootstrapAppendKnowledgeTestCase(unittest.TestCase):
 
     def test_call(self):
         n = Network()
         g = n.register("TEST")
         f = g.register("SOMEFRAME")
 
-        boot = BootstrapKnowledge(n, "TEST.SOMEFRAME", "slot", Literal(123))
+        boot = BootstrapAppendKnowledge(n, "TEST.SOMEFRAME", "slot", Literal(123))
         boot()
 
         self.assertTrue(123 in f["slot"])
