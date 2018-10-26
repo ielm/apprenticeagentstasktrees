3db471ff903e640a8525193ab89f473b886294c4
ielm <leoni@rpi.edu>
2018-10-26 12:29:13 -0400

    WIP - Decide On Language Input functionality

--- backend/resources/AgentMP.py
+++ backend/resources/AgentMP.py
@@ -7,15 +7,21 @@ class FindSomethingToDoMP(AgentMethod):
         self.agent.agenda().add_goal(Goal.instance_of(self.agent.internal, self.agent.exe["ACKNOWLEDGE-INPUT"], []))
 
 
+# ATTN - Not Needed
 class AddGoalMP(AgentMethod):
-    def run(self, tmr):
-        self.agent.agenda().add_goal(Goal.instance_of(self.agent.internal, self.agent.exe["ACKNOWLEDGE-INPUT"], [tmr]))
+    def run(self, input):
+        self.agent.agenda().add_goal(Goal.instance_of(self.agent.internal, self.agent.exe["ACKNOWLEDGE-INPUT"], [input]))
 
 
 class PrintTMR(AgentMethod):
     def run(self, tmr):
         print("\nName: ", tmr.name())
         print("\nISA: ", tmr.concept())
+
+
+class FindTaskMP(AgentMethod):
+    def run(self, tmr):
+        return
 
 
 class AcknowledgeInputMP(AgentMethod):
@@ -25,11 +31,19 @@ class AcknowledgeInputMP(AgentMethod):
 
 class DecideOnLanguageInputMP(AgentMethod):
     def run(self, input_tmr):
+        """
+        Decides whether to RespondToQuery or PerformComplexTask
+        In the future there will be more functionality
+        :param input_tmr:
+        :return: TMR or Task
+        """
+
         # Find LT.BUILD.1
         #     Request Action
         #     Agent = Human
         #     Benef = Robot
         # return LT.BUILD.1
+
         return
 
 
