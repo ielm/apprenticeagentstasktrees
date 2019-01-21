ffa45733dd60c74f0411fbf4bdc1c89612de67f3
Jesse <jesse.english@southerncrossengineering.com>
2019-01-21 10:53:23 -0500

    io ui page

--- backend/service.py
+++ backend/service.py
@@ -470,6 +470,35 @@ def components_graph():
     return render_template("graph.html", gj=json.loads(graph), include_sources=include_sources)
 
 
+@app.route("/io", methods=["GET"])
+def io():
+    from datetime import datetime
+
+    payload = []
+
+    # 1) Gather all of the inputs and outputs
+    payload.extend(agent["INPUTS"].values())
+    payload.extend(agent["OUTPUTS"].values())
+
+    # 2) Map them to the correct XMR objects
+    payload = list(map(lambda frame: XMR.from_instance(frame), payload))
+
+    # 3) Sort by timestamp
+    payload = sorted(payload, key=lambda xmr: xmr.timestamp())
+
+    # 4) Map them to simple dictionaries
+    payload = list(map(lambda xmr: {
+        "type": xmr.type().value,
+        "timestamp": datetime.utcfromtimestamp(xmr.timestamp()).strftime('%H:%M:%S'),
+        "source": xmr.source().name(),
+        "rendered": xmr.render(),
+        "id": xmr.frame.name(),
+        "graph": xmr.graph(agent)._namespace
+    }, payload))
+
+    return render_template("io.html", io=payload)
+
+
 @app.route("/htn", methods=["GET"])
 def htn():
     if "instance" not in request.args:
