c806af5209635c916767ee168384d35bb42605fc
ielm <leoni@rpi.edu>
2019-05-01 15:24:02 -0400

    Add OntoGen environment variables

--- backend/utils/LEIAEnvironment.py
+++ backend/utils/LEIAEnvironment.py
@@ -8,10 +8,18 @@ ONTOLOGY_PORT = int(os.environ["ONTOLOGY_PORT"]) if "ONTOLOGY_PORT" in os.enviro
 ONTOLOGY_DATABASE = os.environ["ONTOLOGY_DATABASE"] if "ONTOLOGY_DATABASE" in os.environ else "leia-ontology"
 ONTOLOGY_COLLECTION = os.environ["ONTOLOGY_COLLECTION"] if "ONTOLOGY_COLLECTION" in os.environ else "robot-v.1.0.0"
 
+ONTOGEN_HOST = os.environ["ONTOGEN_HOST"] if "ONTOGEN_HOST" in os.environ else "localhost"
+ONTOGEN_PORT = os.environ["ONTOGEN_PORT"] if "ONTOGEN_PORT" in os.environ else "5005"
+
 networking = {
     "ontosem-host": os.environ["ONTOSEM_HOST"] if "ONTOSEM_HOST" in os.environ else "localhost",
     "ontosem-port": os.environ["ONTOSEM_PORT"] if "ONTOSEM_PORT" in os.environ else "5000",
 }
 
+
 def ontosem_service():
     return "http://" + networking["ontosem-host"] + ":" + networking["ontosem-port"]
+
+
+def ontogen_service():
+    return "http://" + ONTOGEN_HOST + ":" + ONTOGEN_PORT
