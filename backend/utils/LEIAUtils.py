10142a973dd6fe1486f1d5cae68eaa886a09d133
ielm <leoni@rpi.edu>
2019-05-01 15:24:31 -0400

    Add OntoGen generate utility

---
+++ backend/utils/LEIAUtils.py
@@ -0,0 +1,17 @@
+from LEIAEnvironment import ontogen_service
+from backend.models.tmr import TMR
+from ontograph.Frame import Frame
+
+import requests
+
+
+def ontogen_generate(self, tmr: TMR = None, meta: dict = None):
+    # input = tmr + meta to json
+    if tmr is None:
+        raise Exception("Cannot generate utterance without input TMR")
+    if meta is None:
+        meta = {}
+
+    response = requests.post(url=ontogen_service() + "/generate", data={"meta": meta, "TMR": tmr.frame.dump()})
+
+    return response
