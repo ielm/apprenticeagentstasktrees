611ac2c25da5178c67c51c74cf75ebc23ee1c380
ielm <leoni@rpi.edu>
2019-05-02 16:02:46 -0400

    Updates ontogen generate call and adds test case

--- backend/utils/LEIAUtils.py
+++ backend/utils/LEIAUtils.py
@@ -3,15 +3,18 @@ from backend.models.tmr import TMR
 from ontograph.Frame import Frame
 
 import requests
+import json
 
 
-def ontogen_generate(self, tmr: TMR = None, meta: dict = None):
+def ontogen_generate(tmr, meta: dict = None):
     # input = tmr + meta to json
     if tmr is None:
         raise Exception("Cannot generate utterance without input TMR")
     if meta is None:
         meta = {}
 
-    response = requests.post(url=ontogen_service() + "/gen/api/generate", data={"meta": meta, "TMR": tmr.frame.dump()})
+    data = {'meta': meta, 'TMR': tmr}
+
+    response = requests.post(url=ontogen_service() + "/gen/api/generate", data=json.dumps(data))
 
     return response
---
+++ tests/utils/LEIAUtilsTestCase.py
@@ -0,0 +1,25 @@
+from backend.utils import LEIAUtils
+from ontograph import graph
+from backend.models.tmr import TMR
+
+import unittest
+import json
+import os
+
+
+class OntoGenTestCase(unittest.TestCase):
+
+    @staticmethod
+    def resource(fp):
+        r = None
+        with open(fp) as f:
+            r = json.load(f)
+        return r
+
+    def test_generate_greeting(self):
+        greeting_meta = {}
+        file = os.path.abspath(__package__) + "/../resources/tmrs/HiJake.json"
+        greeting_tmr = self.resource(file)
+
+        output = LEIAUtils.ontogen_generate(greeting_tmr, greeting_meta)
+        print(output)
